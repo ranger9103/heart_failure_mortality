a07aa86a1caeab136132e958b184aaf2d364ee44
Ranger9103 <kateheise@yahoo.com>
2022-08-09 19:51:46 -0500

    aug 9

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
-import scraping
 
 app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+mongo = PyMongo(app, uri = "mongodb://localhost:27017/mars_app")
+
+@app.route("/")
+def home():
+   mars_data = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars_data)
+
+   if __name__ == "__main__":
+    app.run(debug=True)
